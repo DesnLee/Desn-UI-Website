25ad80ca55b98ea3c8733dd2a6e71eb9e7a97850
DesnLee <jiakun.ui@gmail.com>
2022-02-03 15:43:34 +0800

    初始化路由

---
+++ src/components/Haha.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>haha</h1>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import Haha from './components/Haha.vue';
 
-createApp(App).mount('#app')
+const history = createWebHistory();
+const router = createRouter({
+	history,
+	routes: [
+		{ path: '/', component: Haha },
+		{ path: '/index', component: HelloWorld },
+	],
+});
+
+const app = createApp(App);
+app.use(router).mount('#app');
