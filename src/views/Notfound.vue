7e987108e3c64b2c81d37a7f8c64822363f7bafb
DesnLee <jiakun.ui@gmail.com>
2022-02-05 00:15:45 +0800

    创建 404 页面

---
+++ src/views/Notfound.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class = "pic-wrapper">
+      <Icon name = "notfound" />
+    </div>
+    <h2>当前页面不存在，请检查 url</h2>
+    <div class = "link-wrapper">
+      <router-link class = "link" to = "/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang = "ts" setup>
+import Icon from '../components/Icon.vue';</script>
+
+<style lang = "scss" scoped>
+  @import "../assets/style/helper.scss";
+
+  .pic {
+    width: 200px !important;
+    height: 200px !important;
+
+    &-wrapper {
+      text-align: center;
+      margin-top: 48px;
+    }
+  }
+
+  h2 {
+    text-align: center;
+    padding: 48px;
+  }
+
+  .link {
+    color: $color-highlight;
+    border-bottom: 1px solid $color-highlight;
+    font-size: 16px;
+    padding-bottom: 4px;
+    text-align: center;
+
+    &-wrapper {
+      margin-top: 24px;
+      text-align: center;
+    }
+  }
+</style>
