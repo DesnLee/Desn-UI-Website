1fc6b48f04f4b33e4b6aec72cbe3e853e172558d
DesnLee <jiakun.ui@gmail.com>
2022-02-05 00:13:19 +0800

    安装 svg 插件 vite-plugin-svg-icons，配置插件 vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default () => {
+  return {
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [ path.resolve(process.cwd(), 'src/assets/icons') ],
+        // 指定symbolId格式
+        symbolId: '[name]',
+        customDomId: '__svg__icons__dom__',
+      }),
+    ],
+  };
+}
