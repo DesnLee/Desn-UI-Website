9242dac46492f4559e4b8f28ace7b692879853af
DesnLee <jiakun.ui@gmail.com>
2022-02-05 00:16:11 +0800

    完成 NavBar 导航组件

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
-    <div class = "logo-wrapper">
+    <Icon v-if = "!isPC  && currentRoute.indexOf( '/doc') === 0" class = "menu" name = "menu" @click = "toggle" />
+    <router-link class = "logo-wrapper" to = "/">
       <img alt = "" src = "../assets/logo.png" />
-    </div>
-    <span class = "name">DesnLee - UI</span>
+    </router-link>
     <div class = "link-wrapper">
       <router-link to = "/">首页</router-link>
       <router-link to = "/doc">文档</router-link>
@@ -11,36 +11,74 @@
   </nav>
 </template>
 
-<style lang = "scss" scoped>
-  nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    min-width: 1000px;
+<script lang = "ts" setup>
+  import { inject, Ref } from 'vue';
+  import Icon from './Icon.vue';
 
-    .logo-wrapper {
-      padding-left: 48px;
-      margin-right: 12px;
-      height: 96px;
+  const currentRoute = inject<Ref<string>>('currentRoute');
+  const isPC = inject<Ref<boolean>>('isPC');
+
+  let asideVisible = inject<Ref<boolean>>('asideVisible')!;
+  const toggle = () => {
+    asideVisible.value = !asideVisible?.value;
+  };
+</script>
+
+<style lang = "scss" scoped>
+  @import "../assets/style/helper";
+
+  nav {
+    position: sticky;
+    top: 0;
+    width: 100%;
+    height: $height-navbar;
+    background: $color-highlight;
+    display: flex;
+    align-items: center;
+
+    > .logo-wrapper {
+      margin-left: 24px;
+      margin-right: auto;
+      height: $height-navbar;
 
       img {
         height: 100%;
       }
     }
 
-    .name {
-      margin-right: auto;
-      font-size: 24px;
+    > .link-wrapper {
       font-weight: bold;
-    }
-
-    .link-wrapper {
-      font-weight: bold;
-      padding-right: 48px;
+      padding-right: 24px;
 
       > * {
         margin-left: 36px;
+      }
+    }
+
+    .menu {
+      position: absolute;
+      left: 24px;
+      top: 24px;
+      display: none;
+      width: 48px;
+      height: 48px;
+      background: white;
+    }
+
+    @media(max-width: $move-device-width) {
+      & {
+
+        > .menu {
+          display: inline-block;
+        }
+
+        > .link-wrapper {
+          display: none;
+        }
+
+        > .logo-wrapper {
+          margin-left: auto;
+        }
       }
     }
   }
