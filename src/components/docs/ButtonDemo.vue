0d61ec3fc215168fabee458f746ba85a32df420f
DesnLee <jiakun.ui@gmail.com>
2022-02-05 16:57:57 +0800

    初步完成 button 样式

--- src/components/docs/ButtonDemo.vue
+++ src/components/docs/ButtonDemo.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>示例1</h1>
-  <Button size = "normal" @click = "onClick" @focus = "onfocus" @mouseover = "onMouseover">你好</Button>
+  <div>
+    <h1>示例1</h1>
+    <Button size = "small">小型按钮</Button>
+    <Button>默认按钮</Button>
+    <Button size = "large">大型按钮</Button>
+    <Button theme = "button">button</Button>
+    <Button theme = "link">link button</Button>
+    <Button theme = "text">text button</Button>
+  </div>
 </template>
 
 <script lang = "ts" setup>
   import Button from '../../lib/Button.vue';</script>
 
 <style lang = "scss" scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    > * {
+      margin-bottom: 24px;
+    }
+  }
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,25 +1,74 @@
 <template>
-  <div :size = "size">
-    <button v-bind = "rest">
-      <slot />
-    </button>
-  </div>
+  <button :class = "classList" class = "des-button">
+    <slot />
+  </button>
 </template>
 
 <script lang = "ts" setup>
-  import { useAttrs } from 'vue';
+  const props = defineProps({
+    theme: { type: String, default: 'button' },
+    size: { type: String, default: 'normal' }
 
-  const { size, ...rest } = useAttrs();
+  });
+
+  const classList = [
+    `des-button-${ props.theme }`,
+    `des-button-${ props.size }`,
+  ];
 </script>
-<script lang = "ts">
-  export default {
-    inheritAttrs: false
-  };
-</script>
 
-<style lang = "scss" scoped>
+<style lang = "scss">
+  $btn-h-normal: 32px;
 
-  div {
-    border: 1px solid red;
+  $btn-h-small: 24px;
+
+  $btn-h-large: 40px;
+
+  .des-button {
+    display: inline-flex;
+    white-space: nowrap;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #DDD;
+    color: #404244;
+    cursor: pointer;
+    background: #FFF;
+
+    &.des-button-small {
+      font-size: 12px;
+      height: $btn-h-small;
+      border-radius: 3px;
+      padding: {
+        left: $btn-h-small/3;
+        right: $btn-h-small/3;
+      }
+    }
+
+    &.des-button-normal {
+      font-size: 14px;
+      height: $btn-h-normal;
+      border-radius: 4px;
+      padding: {
+        left: $btn-h-normal/3;
+        right: $btn-h-normal/3;
+      }
+    }
+
+    &.des-button-large {
+      font-size: 16px;
+      height: $btn-h-large;
+      border-radius: 5px;
+      padding: {
+        left: $btn-h-large/3;
+        right: $btn-h-large/3;
+      }
+    }
+
+    &:hover {
+      color: #42B983;
+      border: 1px solid #42B983;
+    }
+
+
   }
 </style>
