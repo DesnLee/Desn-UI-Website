947d5e7b95cbf04fbdda3425a618b695412f1452
DesnLee <jiakun.ui@gmail.com>
2022-02-05 16:23:06 +0800

    分离传入的 attrs，并取消默认绑定

--- src/components/docs/ButtonDemo.vue
+++ src/components/docs/ButtonDemo.vue
@@ -1,10 +1,10 @@
 <template>
-  <Button />
+  <h1>示例1</h1>
+  <Button size = "normal" @click = "onClick" @focus = "onfocus" @mouseover = "onMouseover">你好</Button>
 </template>
 
 <script lang = "ts" setup>
-import Button from '../../lib/Button.vue';
-</script>
+  import Button from '../../lib/Button.vue';</script>
 
 <style lang = "scss" scoped>
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,11 +1,25 @@
 <template>
-<div>button样例</div>
+  <div :size = "size">
+    <button v-bind = "rest">
+      <slot />
+    </button>
+  </div>
 </template>
 
 <script lang = "ts" setup>
+  import { useAttrs } from 'vue';
 
+  const { size, ...rest } = useAttrs();
+</script>
+<script lang = "ts">
+  export default {
+    inheritAttrs: false
+  };
 </script>
 
 <style lang = "scss" scoped>
 
+  div {
+    border: 1px solid red;
+  }
 </style>
