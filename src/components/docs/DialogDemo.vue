11183462ed88b69d6a0afa6c983370b04c0a42cc
DesnLee <jiakun.ui@gmail.com>
2022-02-06 00:40:04 +0800

    支持点击显示，支持配置点击蒙版关闭，添加关闭事件，可传入 cancel 和 ok 函数并执行，并检查 ok 函数，返回 true 可关闭 dialog

--- src/components/docs/DialogDemo.vue
+++ src/components/docs/DialogDemo.vue
@@ -1,11 +1,29 @@
 <template>
   <article>
-    <Dialog />
+    <Dialog v-model:visible = "isVisible" :cancel = "functionCancel" :ok = "functionOk" />
+    <Button theme = "link" @click = "toggle">打开 Dialog</Button>
   </article>
 </template>
 
 <script lang = "ts" setup>
-import Dialog from '../../lib/Dialog.vue';</script>
+  import { ref } from 'vue';
+  import Button from '../../lib/Button.vue';
+  import Dialog from '../../lib/Dialog.vue';
+
+  const isVisible = ref(false);
+
+  const toggle = () => {
+    isVisible.value = !isVisible.value;
+  };
+
+  const functionOk = () => {
+    console.log('ok');
+    return true;
+  };
+  const functionCancel = () => {
+    console.log('cancel');
+  };
+</script>
 
 <style lang = "scss" scoped>
 </style>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,37 +1,72 @@
 <template>
-  <div class = "des-dialog-overlay">
-    <div class = "des-dialog-wrapper">
-      <div class = "des-dialog">
-        <div class = "des-dialog-close-icon">
-          <svg class = "icon" height = "16" p-id = "10885" t = "1644070590318" version = "1.1" viewBox = "0 0 1024 1024"
-               width = "200" xmlns = "http://www.w3.org/2000/svg"
-          ><path
-            d = "M92.3 955c-6.4 0-12.9-2.5-17.8-7.4-9.8-9.8-9.8-25.7 0-35.6L912.6 73.9c9.8-9.8 25.7-9.8 35.6 0 9.8 9.8 9.8 25.7 0 35.6L110 947.6c-4.9 4.9-11.3 7.4-17.7 7.4z"
-            fill = "#4F4F4F" p-id = "10886"
-          ></path><path
-            d = "M930.4 955c-6.4 0-12.9-2.5-17.8-7.4L74.5 109.5c-9.8-9.8-9.8-25.7 0-35.6 9.8-9.8 25.7-9.8 35.6 0l838.2 838.2c9.8 9.8 9.8 25.7 0 35.6-5 4.8-11.5 7.3-17.9 7.3z"
-            fill = "#4F4F4F" p-id = "10887"
-          ></path></svg>
+  <template v-if = "visible">
+    <div class = "des-dialog-overlay" @click.self = "clickOverlay">
+      <div class = "des-dialog-wrapper">
+        <div class = "des-dialog">
+          <div class = "des-dialog-close-icon" @click = "close">
+            <svg class = "icon" height = "16" p-id = "10885" t = "1644070590318" version = "1.1"
+                 viewBox = "0 0 1024 1024" width = "200" xmlns = "http://www.w3.org/2000/svg"
+            ><path
+              d = "M92.3 955c-6.4 0-12.9-2.5-17.8-7.4-9.8-9.8-9.8-25.7 0-35.6L912.6 73.9c9.8-9.8 25.7-9.8 35.6 0 9.8 9.8 9.8 25.7 0 35.6L110 947.6c-4.9 4.9-11.3 7.4-17.7 7.4z"
+              fill = "#4F4F4F" p-id = "10886"
+            ></path><path
+              d = "M930.4 955c-6.4 0-12.9-2.5-17.8-7.4L74.5 109.5c-9.8-9.8-9.8-25.7 0-35.6 9.8-9.8 25.7-9.8 35.6 0l838.2 838.2c9.8 9.8 9.8 25.7 0 35.6-5 4.8-11.5 7.3-17.9 7.3z"
+              fill = "#4F4F4F" p-id = "10887"
+            ></path></svg>
+          </div>
+          <header>
+            我是标题
+          </header>
+          <main>
+            <p>我是第一行字</p>
+            <p>我是第二行字</p>
+          </main>
+          <footer>
+            <Button level = "lesser" theme = "text" @click = "clickCancel">Cancel</Button>
+            <Button @click = "clickOk">OK</Button>
+          </footer>
         </div>
-        <header>
-          我是标题
-        </header>
-        <main>
-          <p>我是第一行字</p>
-          <p>我是第二行字</p>
-        </main>
-        <footer>
-          <Button level = "lesser" theme = "text">Cancel</Button>
-          <Button>OK</Button>
-        </footer>
       </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang = "ts" setup>
+import Button from './Button.vue';
 
-import Button from './Button.vue';</script>
+const props = defineProps({
+  visible: { type: Boolean, default: false },
+  closeOnOverlay: { type: Boolean, default: true },
+  ok: Function,
+  cancel: Function,
+});
+
+const emit = defineEmits([ 'update:visible' ]);
+const close = () => {
+  emit('update:visible', false);
+};
+
+const clickOverlay = () => {
+  if (props.closeOnOverlay) {
+    close();
+  }
+};
+
+const clickOk = () => {
+  if (props.ok) {
+    if (props.ok()) close();
+  } else {
+    close();
+  }
+};
+
+const clickCancel = () => {
+  if (props.cancel) {
+    props.cancel();
+  }
+  close();
+};
+</script>
 
 <style lang = "scss">
   .des-dialog-overlay {
@@ -44,6 +79,7 @@ import Button from './Button.vue';</script>
     background: #00000033;
 
     > .des-dialog-wrapper {
+      z-index: 20;
       border-radius: 8px;
       overflow: hidden;
       box-shadow: 0 0 24px #00000020;
