66e871024084f532a0e36eec383603dd9b945dcb
DesnLee <jiakun.ui@gmail.com>
2022-02-06 23:31:48 +0800

    调整 html 结构，并添加 css， 初步完成首页 css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,41 @@
     <div class = "banner">
       <div class = "banner-content">
         <h1>Des UI</h1>
-        <h3>一个极简的 Vue.js v3 UI 组件库</h3>
+        <h3>一个极简的 Vue.js 3.0 UI 组件库</h3>
         <p class = "actions">
-          <a class = "link-lesser" href = "https://github.com" target = "_blank">Github</a>
-          <router-link class = "link-primary" to = "/doc/start">起步</router-link>
+          <a class = "link-lesser" href = "https://github.com" target = "_blank">
+            <Icon name = "github" />
+            <span>Github</span>
+          </a>
+          <router-link class = "link-primary" to = "/doc/start">
+            <span>起步</span>
+            <Icon name = "start" />
+          </router-link>
         </p>
       </div>
   </div>
   </div>
+  <section class = "home-part2">
+    <div>
+      <Icon name = "vue" />
+      <h5>基于 Vue.js 3.0</h5>
+      <p>使用了 Vue.js 3.0 的 Composition API 制作</p>
+    </div>
+    <div>
+      <Icon name = "ts" />
+      <h5>基于 TypeScript</h5>
+      <p>源代码采用 TypeScript（非严格检查）</p>
+    </div>
+    <div>
+      <Icon name = "easy" />
+      <h5>代码易读</h5>
+      <p>每个组件的源代码都极其简单</p>
+    </div>
+  </section>
 </template>
 
 <script lang = "ts" setup>
+import Icon from '../components/Icon.vue';
 import NavBar from '../components/NavBar.vue';</script>
 
 <style lang = "scss" scoped>
@@ -49,13 +73,35 @@ import NavBar from '../components/NavBar.vue';</script>
         }
 
         > .actions {
+          display: flex;
           margin-top: 64px;
+
+          .link-lesser, .link-primary {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 8px;
+            padding: 12px 0;
+            width: 128px;
+            transition: all .3s;
+            font-size: 16px;
+            line-height: 20px;
+
+            > svg {
+              width: 20px;
+              height: 20px;
+            }
+          }
 
           .link-primary {
             background: #42B983;
             margin-left: 24px;
             color: #FFFFFFDD;
             font-weight: bold;
+
+            > svg {
+              margin-left: 8px;
+            }
 
             &:hover {
               background: #42B98399;
@@ -67,20 +113,51 @@ import NavBar from '../components/NavBar.vue';</script>
             border: 1px solid #42B983;
             color: #2AA36C;
 
+            > svg {
+              margin-right: 8px;
+            }
+
             &:hover {
               background: #42B98333;
             }
           }
-
-          .link-lesser, .link-primary {
-            display: inline-block;
-            text-align: center;
-            border-radius: 8px;
-            padding: 12px 0;
-            width: 96px;
-            transition: all .3s;
-          }
         }
+      }
+    }
+  }
+
+  .home-part2 {
+    max-width: 1000px;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    padding: 48px 0;
+
+    > div {
+      width: 33.333%;
+      padding-left: 5%;
+      padding-right: 5%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      > svg {
+        margin-bottom: 24px;
+        width: 44px;
+        height: 44px;
+      }
+
+      > h5 {
+        font-size: 18px;
+        color: #404244;
+      }
+
+      > p {
+        color: #404244;
+        font-weight: 300;
+        font-size: 14px;
+
       }
     }
   }
