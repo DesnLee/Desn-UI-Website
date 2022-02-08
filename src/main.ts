054306441f20e02926930e025b0573bf743c2383
DesnLee <jiakun.ui@gmail.com>
2022-02-08 14:24:04 +0800

    将 highlight 包全量引入改为指定引入，以降低 build 包大小

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,28 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'virtual:svg-icons-register';
-import hljs from 'highlight.js';
 
+// 引入 highlight 核心和语言
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import xml from 'highlight.js/lib/languages/xml';
+import shell from 'highlight.js/lib/languages/shell';
+
+// 引入全局、markdown 格式化、代码高亮的 CSS
 import './assets/style/reset.scss';
 import './lib/index.scss';
 import 'github-markdown-css';
 import './assets/style/highlight.scss';
 
+// 注册 highlight 语言
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('shell', shell);
+
+// 创建 Vue App
 const app = createApp(App);
 
+// 注册全局 highlight 指令
 app.directive('highlight', function (el) {
   const blocks = el.querySelectorAll('pre code');
   blocks.forEach((block: any) => {
@@ -18,4 +31,5 @@ app.directive('highlight', function (el) {
   });
 });
 
+// 挂载 App 到 DOM
 app.use(router).mount('#app');
