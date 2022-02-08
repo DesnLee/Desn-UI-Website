98c35fd58cf7305f83bae51587c91032812d893d
DesnLee <jiakun.ui@gmail.com>
2022-02-08 12:59:26 +0800

    引入 highlight.js，添加自定义指令以调用 highlight

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import 'virtual:svg-icons-register';
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
+import 'virtual:svg-icons-register';
+import hljs from 'highlight.js';
+
 import './assets/style/reset.scss';
 import './lib/index.scss';
 import 'github-markdown-css';
-import router from './router';
+import './assets/style/highlight.scss';
 
 const app = createApp(App);
+
+app.directive('highlight', function (el) {
+  const blocks = el.querySelectorAll('pre code');
+  blocks.forEach((block: any) => {
+    hljs.highlightElement(block);
+  });
+});
+
 app.use(router).mount('#app');
