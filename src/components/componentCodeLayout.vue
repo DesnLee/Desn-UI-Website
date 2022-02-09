f9ddc7d94f1770ecbf70b21a48648a99b67c43d0
DesnLee <jiakun.ui@gmail.com>
2022-02-10 00:46:47 +0800

    markdown 样式移入 global.scss，为 componentCodeLayout 添加样式

--- src/components/componentCodeLayout.vue
+++ src/components/componentCodeLayout.vue
@@ -25,18 +25,58 @@
   const switchCodeVisible = () => {
     codeVisible.value = !codeVisible.value;
   };
-  
+
   const btnText = computed(() => {
     return codeVisible.value ? '隐藏代码' : '查看代码';
   });
 </script>
 
 <style lang = "scss" scoped>
-.code-enter-active, .code-leave-active {
-  transition: all .3s ease-out;
-}
+  .components-h1 {
+    font-size: 32px;
+  }
 
-.code-enter-from, .code-leave-to {
-  opacity: 0;
-}
+  .components-wrapper {
+    line-height: 1;
+    border: 1px solid #EEE;
+    border-radius: 6px;
+    margin-top: 24px;
+    padding: 12px;
+
+    > .components-h2 {
+      margin-top: 8px;
+      font-size: 20px;
+    }
+
+    > .components-demo {
+      margin: 16px 0 0 0;
+      padding: 24px 0 0 0;
+      border-top: 1px solid #EEE;
+
+      > .component-instance {
+        display: flex;
+        align-items: flex-end;
+      }
+
+      > .showCode {
+        display: block;
+        margin-top: 24px;
+      }
+    }
+
+    > .components-code {
+      border-top: 1px solid #EEE;
+      margin-top: 24px;
+      font-size: 12px;
+      line-height: 1.8;
+    }
+  }
+
+  .code-enter-active, .code-leave-active {
+    transition: all .3s ease-out;
+  }
+
+  .code-enter-from, .code-leave-to {
+    opacity: 0;
+  }
 </style>
