aebf6de6dfdbce80a62753c0441fd5c22da35bfb
DesnLee <jiakun.ui@gmail.com>
2022-02-10 00:27:05 +0800

    将 Switch 组件展示页使用 组件展示layout 进行展示

--- src/components/componentList/SwitchDemo.vue
+++ src/components/componentList/SwitchDemo.vue
@@ -1,43 +1,61 @@
 <template>
   <article class = "component-article-container article-container">
-    <div class = "switch-wrapper">
-      <Switch v-model:value = "largeValue" :text = "cnText" size = "large" />
-      <span>大按钮（加文字）</span>
-    </div>
-    <div class = "switch-wrapper">
-      <Switch v-model:value = "normalValue" size = "normal" />
-      <span>中按钮</span>
-    </div>
-    <div class = "switch-wrapper">
-      <Switch v-model:value = "smallValue" color = "#ff0000" size = "small" />
-      <span>小按钮（改颜色）</span>
-    </div>
+    <h1 class = "components-h1">Switch 组件</h1>
+    <ComponentCodeLayout>
+      <template #title>
+        常规 Switch
+      </template>
+      <template #component>
+        <component :is = "Switch1" />
+      </template>
+      <template #code>
+        {{ Switch1Code }}
+      </template>
+    </ComponentCodeLayout>
+    <ComponentCodeLayout>
+      <template #title>
+        带文字 Switch
+      </template>
+      <template #component>
+        <component :is = "Switch2" />
+      </template>
+      <template #code>
+        {{ Switch2Code }}
+      </template>
+    </ComponentCodeLayout>
+    <ComponentCodeLayout>
+      <template #title>
+        修改颜色 Switch
+      </template>
+      <template #component>
+        <component :is = "Switch3" />
+      </template>
+      <template #code>
+        {{ Switch3Code }}
+      </template>
+    </ComponentCodeLayout>
   </article>
 </template>
 
 <script lang = "ts" setup>
   import { ref } from 'vue';
-  import Switch from '../../lib/Switch.vue';
+  import ComponentCodeLayout from '../componentCodeLayout.vue';
 
-  const largeValue = ref<boolean>(false);
-  const normalValue = ref<boolean>(false);
-  const smallValue = ref<boolean>(false);
-  const cnText = {
-    'true': '开启',
-    'false': '关闭'
+  import Switch1 from '../componentDemo/Switch1.vue';
+  import Switch2 from '../componentDemo/Switch2.vue';
+  import Switch3 from '../componentDemo/Switch3.vue';
+  import Switch1Code from '../componentDemo/Switch1.vue?raw';
+  import Switch2Code from '../componentDemo/Switch2.vue?raw';
+  import Switch3Code from '../componentDemo/Switch3.vue?raw';
+
+  const codeVisible = ref([ false, false, false ]);
+
+  const switchCodeVisible = (index) => {
+    codeVisible.value[index] = !codeVisible.value[index];
   };
 </script>
 
 <style lang = "scss" scoped>
-  .switch-wrapper {
-    display: flex;
-    align-items: center;
-    margin-bottom: 36px;
 
-    > span {
-      margin-left: 36px;
-      color: #404244;
-      font-size: 16px;
-    }
-  }
+
 </style>
