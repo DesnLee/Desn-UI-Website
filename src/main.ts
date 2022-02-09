66ead5359f9f327c5953055436e0949910640316
DesnLee <jiakun.ui@gmail.com>
2022-02-10 00:47:55 +0800

    引入 global.scss

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import 'virtual:svg-icons-register';
 
 // 引入全局、markdown 格式化、代码高亮的 CSS
 import './assets/style/reset.scss';
-import './lib/index.scss';
-import 'github-markdown-css';
+import './assets/style/global.scss';
 import './assets/style/highlight.scss';
+import 'github-markdown-css';
+import './lib/index.scss';
 
 // 引入自定义指令
 import highlight from './directives/highlight';
