3a4f65ed1d7eab0f5ec3b6ccfde36756cee24c4d
DesnLee <jiakun.ui@gmail.com>
2022-02-09 15:18:13 +0800

    使用 h 函数动态渲染 Markdown 组件

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { h } from 'vue';
 
 import ButtonDemo from './components/componentList/ButtonDemo.vue';
 import DialogDemo from './components/componentList/DialogDemo.vue';
 import SwitchDemo from './components/componentList/SwitchDemo.vue';
 import TabsDemo from './components/componentList/TabsDemo.vue';
-
-import Install from './components/docs/Install.vue';
-import Introduction from './components/docs/Introduction.vue';
-import Start from './components/docs/Start.vue';
+import Markdown from './components/Markdown.vue';
 
 import Doc from './views/Doc.vue';
 import Home from './views/Home.vue';
@@ -22,9 +20,18 @@ export default createRouter({
       path: '/doc',
       component: Doc,
       children: [
-        { path: 'start', component: Start },
-        { path: 'install', component: Install },
-        { path: 'introduction', component: Introduction },
+        {
+          path: 'start',
+          component: h(Markdown, { name: 'start', key: 'start' })
+        },
+        {
+          path: 'install',
+          component: h(Markdown, { name: 'install', key: 'install' })
+        },
+        {
+          path: 'introduction',
+          component: h(Markdown, { name: 'introduction', key: 'introduction' })
+        },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
