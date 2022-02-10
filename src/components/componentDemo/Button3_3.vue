f480b3e3f2925c1f00440005b70d43c72e23c0fa
DesnLee <jiakun.ui@gmail.com>
2022-02-10 09:34:10 +0800

    删除多余组件

--- src/components/componentDemo/Button3_3.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Button level = "primary" theme = "text">主要 text</Button>
-  <Button level = "lesser" theme = "text">次要 text</Button>
-  <Button level = "dangerous" theme = "text">危险 text</Button>
-</template>
-
-<script lang = "ts" setup>
-  import Button from '../../lib/Button.vue';</script>
