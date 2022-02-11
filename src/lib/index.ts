972dba7b5a5afb75af381626bef924f30b6fd8b0
DesnLee <jiakun.ui@gmail.com>
2022-02-11 12:04:37 +0800

    移除组件库

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Switch } from './components/Switch.vue';
-export { default as Button } from './components/Button.vue';
-export { default as Dialog } from './components/Dialog.vue';
-export { default as Tab } from './components/Tab.vue';
-export { default as Tabs } from './components/Tabs.vue';
-export { openDialog as openDialog } from './components/openDialog';
